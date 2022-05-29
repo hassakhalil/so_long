f585a04297d9852a2b8cdb02e2f1316623782199
hassan khalil <hkhalil@student.1337.ma>
2022-05-29 22:54:32 +0100

    coding the start of the game

--- main.c
+++ main.c
@@ -14,7 +14,7 @@
 
 int main(int argc, char *argv[])
 {
-    t_data  img;
+    t_data  game;
     int     n = 0;
     int     m = 0;
     int     i = 0;
@@ -28,12 +28,12 @@ int main(int argc, char *argv[])
         write(2, "Error\n", 7);
         return (-1); 
     }
-    img.mlx = mlx_init();
+    game.mlx = mlx_init();
     m = n_columns(argv[1]);
     n = n_lines(argv[1]);
-    img.mlx_win = mlx_new_window(img.mlx, m * 50, n * 50, "./so_long");
-    img.img = mlx_new_image(img.mlx, m * 50, n * 50);
-    img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length, &img.endian);
+    game.mlx_win = mlx_new_window(game.mlx, m * 50, n * 50, "./so_long");
+    game.img = mlx_new_image(game.mlx, m * 50, n * 50);
+    game.addr = mlx_get_data_addr(game.img, &game.bits_per_pixel, &game.line_length, &game.endian);
     fd = open(argv[1], O_RDWR);
     if (fd == -1)
         return (-1);
@@ -44,27 +44,27 @@ int main(int argc, char *argv[])
         line = get_next_line(fd);
         while (j < m * 50)
         {
-            img.img = mlx_xpm_file_to_image(img.mlx, "xpm/background.xpm", &img.img_width, &img.img_height);
-            mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, j, i);
+            game.img = mlx_xpm_file_to_image(game.mlx, "xpm/background.xpm", &game.img_width, &game.img_height);
+            mlx_put_image_to_window(game.mlx, game.mlx_win, game.img, j, i);
             if (line[l] == '1')
             {
-                img.img = mlx_xpm_file_to_image(img.mlx, "xpm/wall.xpm", &img.img_width, &img.img_height);
-                mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, j, i);
+                game.img = mlx_xpm_file_to_image(game.mlx, "xpm/wall.xpm", &game.img_width, &game.img_height);
+                mlx_put_image_to_window(game.mlx, game.mlx_win, game.img, j, i);
             }
             else if (line[l] == 'C')
             {
-                img.img = mlx_xpm_file_to_image(img.mlx, "xpm/coin.xpm", &img.img_width, &img.img_height);
-                mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, j, i);
+                game.img = mlx_xpm_file_to_image(game.mlx, "xpm/coin.xpm", &game.img_width, &game.img_height);
+                mlx_put_image_to_window(game.mlx, game.mlx_win, game.img, j, i);
             }
             else if (line[l] == 'E')
             {
-                img.img = mlx_xpm_file_to_image(img.mlx, "xpm/exit.xpm", &img.img_width, &img.img_height);
-                mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, j, i);
+                game.img = mlx_xpm_file_to_image(game.mlx, "xpm/exit.xpm", &game.img_width, &game.img_height);
+                mlx_put_image_to_window(game.mlx, game.mlx_win, game.img, j, i);
             }
             else if (line[l] == 'P')
             {
-                img.img = mlx_xpm_file_to_image(img.mlx, "xpm/position.xpm", &img.img_width, &img.img_height);
-                mlx_put_image_to_window(img.mlx, img.mlx_win, img.img, j, i);
+                game.img = mlx_xpm_file_to_image(game.mlx, "xpm/position.xpm", &game.img_width, &game.img_height);
+                mlx_put_image_to_window(game.mlx, game.mlx_win, game.img, j, i);
             }
             l++;
             j += 50;
@@ -73,6 +73,6 @@ int main(int argc, char *argv[])
         i += 50;
     }
     close(fd);
-    mlx_loop(img.mlx);
+    mlx_loop(game.mlx);
     return (0);
 }
