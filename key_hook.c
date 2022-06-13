19e8b6235142ee7713d370ce4cba17ddbe8f507a
hassan khalil <hkhalil@student.1337.ma>
2022-06-13 01:26:26 +0100

    norm

--- key_hook.c
+++ key_hook.c
@@ -6,15 +6,15 @@
 /*   By: hkhalil <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/30 03:30:35 by hkhalil           #+#    #+#             */
-/*   Updated: 2022/06/12 04:53:38 by hkhalil          ###   ########.fr       */
+/*   Updated: 2022/06/13 01:25:32 by hkhalil          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-void    new_position(t_data *game, int keycode)
+void	new_position(t_data *game, int keycode)
 {
-    if (keycode == 13)
+	if (keycode == 13)
 	{
 		((*game).ny) = ((*game).y) - 1;
 		((*game).nx) = (*game).x;
@@ -36,10 +36,21 @@ void    new_position(t_data *game, int keycode)
 	}
 }
 
-void    free_exit(t_data *game)
+void	free_exit(t_data *game)
 {
-    free_game(game);
+	free_game(game);
 	exit(0);
+}
+
+void	last_move(t_data *game, int collectibles, int *moves)
+{
+	if (collectibles)
+		return ;
+	ft_putnbr_fd(++(*moves), 1);
+	write(1, "\n", 1);
+	((*game).map)[((*game).y)][((*game).x)] = '0';
+	map_draw(game);
+	free_exit(game);
 }
 
 int	key_hook(int keycode, t_data *game)
@@ -48,14 +59,15 @@ int	key_hook(int keycode, t_data *game)
 	static int	collectibles = 0;
 
 	if (keycode == 53)
-        free_exit(game);
+		free_exit(game);
 	if (!collectibles)
 		collectibles = number_of_collectibles(game);
 	position(game);
-    new_position(game, keycode);
+	new_position(game, keycode);
 	if (((*game).map)[((*game).ny)][((*game).nx)] == '1')
 		return (0);
-	if (((*game).map)[((*game).ny)][((*game).nx)] == '0' || ((*game).map)[((*game).ny)][((*game).nx)] == 'C')
+	if (((*game).map)[((*game).ny)][((*game).nx)] == '0'
+		|| ((*game).map)[((*game).ny)][((*game).nx)] == 'C')
 	{
 		ft_putnbr_fd(++moves, 1);
 		write(1, "\n", 1);
@@ -66,14 +78,6 @@ int	key_hook(int keycode, t_data *game)
 		map_draw(game);
 	}
 	else
-	{
-		if (collectibles)
-			return (0);
-		ft_putnbr_fd(++moves, 1);
-		write(1, "\n", 1);
-		((*game).map)[((*game).y)][((*game).x)] = '0';
-		map_draw(game);
-		free_exit(game);
-	}
+		last_move(game, collectibles, &moves);
 	return (0);
 }
